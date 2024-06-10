feb287fb10826649e30e6cebeff953d17fc87479
Senthil A <senthilabi7781@gmail.com>
2024-06-10 10:27:37 +0100

    monday morning ,
    rectivified errors, corrected the relevant df , added comparison chart

--- FBDv2.py
+++ FBDv2.py
@@ -1,4 +1,4 @@
-#version 2 of the dashboard desing with the inputs 6 th June
+#version 2 of the dashboard design with the inputs 8 th June
 #importing necesary libraires
 
 import streamlit as st 
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go 
 import plotly.express as px 
 from st_aggrid import AgGrid ,GridUpdateMode
+from st_aggrid.grid_options_builder import GridOptionsBuilder 
 
 #Setting the layout
 
@@ -17,34 +18,20 @@ st.set_page_config(page_title='Player DashBoard',page_icon=':soccer:',
 
 
 #Loading the dataset
-#import base64
-
-#@st.cache_data()
-#def get_base64_of_bin_file(bin_file):
-#    with open(bin_file, 'rb') as f:
-#        data = f.read()
-#    return base64.b64encode(data).decode()
-
-#def set_png_as_page_bg(png_file):
-#    bin_str = get_base64_of_bin_file(png_file)
-#   page_bg_img = '''
-#    <style>
-#    body {
-#    background-image: url("data:image/png;base64,%s");
-#    background-size: cover;
-#    }
-#    </style>
-#    ''' % bin_str
-#    #st.write('Hello inside ')
-#    st.markdown(page_bg_img, unsafe_allow_html=True)
-#    return
-#bgfile='player_images/Bruma.png'
-#set_png_as_page_bg(bgfile)
-
 @st.cache_resource
 def data_load ( filename):
     return pd.ExcelFile(filename)
+
+#Data sources
+#Absolute path
+#"C:\Users\senth\Documents\GitHub\Pandas_practice\Data_Dashboard_Extended.xlsx"
+# Player data
 excel_file='Player Data2.xlsx'
+# Extended data list for subbar graphs
+file2='dd2.xlsx'
+
+
+#Extraction
 xl=data_load(excel_file)
 
 xlsheets=xl.sheet_names
@@ -72,20 +59,23 @@ col=df.columns
 df[col[1:]]=df[col[1:]].round(2)
 df.sort_values(by=['TSP Score'],inplace=True,ascending=False)
 df=df.reset_index(drop=True)
-#st.write(df.columns)
-#st.table(df)
+
 player_list=df['Player'].to_list()
  
-file2='dd2.xlsx'
-#"C:\Users\senth\Documents\GitHub\Pandas_practice\Data_Dashboard_Extended.xlsx"
+#Extended data set
 xl2=data_load(file2)
 xl2sheets=xl2.sheet_names
+#Loading of multiindex excel data
 techdf=pd.read_excel(file2,sheet_name=xl2sheets[0],
                     header=[0,1])
 tactdf=pd.read_excel(file2,sheet_name=xl2sheets[1],
                     header=[0,1])
-techcol=techdf.columns.get_level_values
-c=tactdf.columns.get_level_values(1)
+#methods to get the column value of multi index data frame
+#techcol=techdf.columns.get_level_values # Not required
+#c=tactdf.columns.get_level_values(1) # Not required
+
+# Assigning the player column , the frist column of the df to index and converting the tuple in index to values
+
 techdf.set_index('Unnamed: 0_level_0', inplace=True)
 
 techdf.index=techdf.index.map(lambda x :  x[0])
@@ -93,22 +83,24 @@ techdf.index=techdf.index.map(lambda x :  x[0])
 tactdf.set_index('Unnamed: 0_level_0', inplace=True)
 
 tactdf.index=tactdf.index.map(lambda x :  x[0])
-#st.write(tactdf.index)
+
+# Functions
+
 #code for radar chart
 
-def radar_chart1(df2):
-
-    categories=list(df2.columns[1:])
+def radar_chart1(df):
+    #st.write(df) #not required
+    categories=list(df.columns[1:])
 
     fig = go.Figure()
     config ={'scrollzoom': True}
 
-    for i in range(df2.shape[0]):
+    for i in range(df.shape[0]):
         fig.add_trace(go.Scatterpolar(
-          r=list(df2.iloc[i,1:]),
+          r=list(df.iloc[i,1:]),
           theta=categories,
           fill='toself',
-          name=df2.iloc[i,0]
+          name=df.iloc[i,0]
     ))
     fig.update_layout(
       height=500,
@@ -120,36 +112,13 @@ def radar_chart1(df2):
         ),
       ),
       
-      showlegend=False
+      showlegend=True
     )
 
     return  fig
 
+#Function for Circle score :
 
-
-#renaming the scores sheet for simplicity and extracting score of selected player
-dfcs=circle_score_data
-#circle_score = dfcs[(dfcs['Player']==selected_player)].iloc[0,1]
-#st.sidebar.plotly_chart(gauge(circle_score),height=100)
-#st.write(circle_score)
-#st.write(selected_player)
-# CSS Code for changing the alignment of score card , but no effect
-st.sidebar.markdown(
-    """
-    <style>
-    .sidebar .sidebar-content {
-        padding-top: 0px;  /* Reduce spacing from the top */
-    }
-    .sidebar .stPlotlyChart {
-        margin-bottom: 0px;  /* Reduce spacing below the Plotly chart */
-    }
-    .sidebar .stMultiSelect {
-        margin-top: 0px;  /* Reduce spacing above the multi-selection widget */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 def gauge(circle_score):
     fig_circle = go.Figure(go.Indicator(
     mode="gauge+number",
@@ -175,6 +144,9 @@ def gauge(circle_score):
     width=800,
     height=300)
     return fig_circle
+
+#Function fot the Bar Charts:
+
 def bar_chart(series,name):
     fig = go.Figure()
     fig.add_trace(go.Bar(
@@ -194,54 +166,112 @@ def bar_chart(series,name):
         #text=list(series))
     return fig
 
+def comparison_bar_chart(series1, series2, name1,name2):
+    fig = go.Figure()
 
-middf=Mid_text_area_data
-middf['Contract']=middf['Contract'].astype(str)
-middf['TSP Score']=(middf['TSP Score']*100).round(0)
-middf['Age']=middf['Age'].astype(str)
+    # Add trace for the first player
+    fig.add_trace(go.Bar(
+        y=series1.index,
+        x=list(series1),
+        name=name1,
+        orientation='h',
+        text=list(series1),
+        textposition='inside',
+        textangle=0,
+        insidetextanchor='middle',
+        textfont={'size': 20},
+        marker=dict(color='blue')  # Color for the first player
+    ))
+
+    # Add trace for the second player
+    fig.add_trace(go.Bar(
+        y=series2.index,
+        x=list(series2),
+        name=name2,
+        orientation='h',
+        text=list(series2),
+        textposition='inside',
+        textangle=0,
+        insidetextanchor='middle',
+        textfont={'size': 20},
+        marker=dict(color='orange')  # Color for the second player
+    ))
+
+    fig.update_layout(
+        barmode='group',  # Use 'group' mode to place bars side-by-side
+        height=600,
+        title='Player Comparison',
+        xaxis=dict(title='Value'),
+        yaxis=dict(title='Attributes')
+    )
+
+    return fig
+
+
+
+
+##renaming the scores sheet for simplicity and extracting score of selected player
+dfcs=circle_score_data
+#circle_score = dfcs[(dfcs['Player']==selected_player)].iloc[0,1]
+#st.sidebar.plotly_chart(gauge(circle_score),height=100)
+#st.write(circle_score)
+#st.write(selected_player)
+
+#player selection df from the mid table area data
+psdf=Mid_text_area_data
+
+psdf['Contract']=psdf['Contract'].astype(str)
+psdf['TSP Score']=(psdf['TSP Score']*100).round(0)
+psdf['Age']=psdf['Age'].astype(str)
+
 #middf.drop(9)
-middf1=middf[['Player','TSP Score']]
+#selcting only the first 2 columns
+psdf1=psdf[['Player','TSP Score']]
 
 
-from st_aggrid.grid_options_builder import GridOptionsBuilder 
-gd=GridOptionsBuilder.from_dataframe(middf1)
+# code for AG grid display
+gd=GridOptionsBuilder.from_dataframe(psdf1)
 gd.configure_pagination(enabled=False)
 gd.configure_selection(selection_mode='single',use_checkbox=True)
+
+# Default player in the list to show the score when the page loads
 selplayers= ['Ideal Left Winger']
 
-#from PIL import Image
-#input_image = Image.open('player_images/club_logo.jpg')
-#st.image(input_image,width=200)
-c1,c2=st.sidebar.columns(2)
-with c1:
-    st.sidebar.image('player_images/club_logo.jpg',width=100)
 
-    st.sidebar.title('Choose your player:soccer:')
+# Dashboard screen starts here
+
+st.sidebar.image('player_images/club_logo.jpg',width=100)
+
+st.sidebar.title('Choose your player:soccer:')
 with st.sidebar:
-    #st.sidebar.subheader(selected_player)
-    mid_table=AgGrid(middf1.drop(9), gridOptions=gd.build(),
+    
+    stable=AgGrid(psdf1.drop(9), gridOptions=gd.build(),
                  update_mode=GridUpdateMode.SELECTION_CHANGED,
                   height=300,
                    )
-
+# try used to by pass if there is no selection
 try:
-    selp2=mid_table['selected_rows'].iloc[0,0]
+    selp2=stable['selected_rows'].iloc[0,0]
     selplayers.insert(0,selp2)
 except:
     pass
-#selplayers.append(selp2)
 
+# Log of the firm
 st.sidebar.image('player_images/SmartScoutlogo.png',width=200)
+
+# filtering the df with selected players
 sp_details=df[df['Player'].isin(selplayers)]
+
 #text area starts here
+
 tdf=text_area_data
-t1=tdf.columns[0]
+t1=tdf.columns[0] 
 t2=tdf.iloc[0,0]
 t3=tdf.columns[1]
 t4=selplayers[0]
 #t4=tdf.iloc[0,1]
-t5='TSP Score'
-t6=middf[middf['Player']==selplayers[0]].iloc[0,5]
+#t5='TSP Score'
+t6=psdf[psdf['Player']==selplayers[0]].iloc[0,5]
 #st.write(t6)
 #top_row=['t'+str(i) for i in range(1,6)]
 #top_row=[t1,t2,t3,t4,t5,t6]
@@ -252,14 +282,22 @@ second_row.insert(0,tdf.columns[2].split()[0])
 
 third_row=tdf[tdf.columns[3]].tolist()
 third_row.insert(0,tdf.columns[3].split()[0])
+
+#removing the nan values in the columns 4,5,6 with blank space
 for i in range(4,7):
     third_row[i]=''
 
 #fourth_row=[tdf.columns[4],tdf.iloc[0,4],tdf.columns[5],tdf.iloc[0,5]]
-fourth_row=[tdf.columns[4],'',tdf.columns[5],'']
+#replacing the coming soon with blank spaces
+fourth_row=[tdf.columns[4],'18',tdf.columns[5],'7']
 fifth_row=['Team Scouting For:','Sunderland A.F.C']
+
+#list of bar for the  plot
+
 plot_rows=[fourth_row,third_row,second_row,top_row,fifth_row]
 
+
+#dynamic width for the plot 
 values=[]
 for i in plot_rows:
     l=[round(100/len(i),2) for j in range(len(i))]
@@ -267,18 +305,18 @@ for i in plot_rows:
 
 # Normalize the values to represent percentages
 normalized_values = []
-for student_values in values:
-    total = sum(student_values)
-    percentages = [value / total * 100 for value in student_values]
+for value in values:
+    total = sum(value)
+    percentages = [x / total * 100 for x in value]
     normalized_values.append(percentages)
 
 # Define colors for each attribute
-#colors = ['#636EFA', '#EF553B', '#00CC96','#112277','#225577','#004488','#2266AA']
-colors=[['lightblue','white'],
-        ['lightblue','white','lightblue','white','lightblue','white'],
+
+colors=[['lightblue','white','lightblue','white'],
+        ['lightblue','white','white','white','white','white','white'],
         ['lightblue','white','white','white','white','white','white','white'],
         ['lightblue','white','white','white','white','white','white'],   
-        ['lightblue','white','lightblue','white','lightblue','white']]
+        ['lightblue','white']]#,'lightblue','white']]
 # Create the figure
 fig = go.Figure()
 config ={'displayModeBar': False}
@@ -287,7 +325,7 @@ config ={'staticplot': True}
 for i, bar_values in zip(range(5), normalized_values):
     
     for bar, percentage, color in zip(plot_rows[i], bar_values, colors[i]):
-        
+        get_font_size=lambda color: 60 if color == 'lightblue' else 20
         fig.add_trace(go.Bar(
             y=[i],  # Single bar for each row
             x=[percentage],  # The width of the segment
@@ -295,7 +333,11 @@ for i, bar_values in zip(range(5), normalized_values):
             orientation='h',
             text=f"{bar}",#: {percentage:.1f}%",  # Text inside the bar
             textposition='inside',
-            textfont_size=30,
+            textangle=0,
+            
+            textfont_size=get_font_size(color),
+                               
+            #textfont_size=30,
             insidetextanchor='middle',  # Center-align the text inside the bars
             marker=dict(color=color),  # Set the color for each segment
             hoverinfo='none'
@@ -326,36 +368,40 @@ config ={'displayModeBar': False}
 # Display the chart
 st1=st.expander(label='',expanded=True)
 col1 ,col2, col3 =st1.columns([3,1,1])
+
+#top dahsboard chart
 with col1:
        st.plotly_chart(fig,config=config)
-
+#player image
 with col2:
     if selplayers[0]=='Ideal Left Winger':
+        # 'for' block is for the alignment of the image
         for i in range(6):
             st.write(' ')
         st.image('player_images/SmartScoutlogo.png',width=150)
 
     else:      
         pl_filename=selplayers[0].replace(' ','_')
-        player_img1='player_images/'+pl_filename+'.png'
-        #player_img='player_images/Andreas_S_Olsen.png'
-        #st.write(player_img)
-        #st.write(player_img1)
-        #from PIL import Image
-        #input_image = Image.open(player_img1)
-        #st.image(input_image,width=200)
-        #st.image('player_images/Antonia_Nusa.png',width=200)
+        player_img1='player_images/'+pl_filename+'.png'        
         st.image(player_img1,width=200)
-
         st.subheader(selplayers[0])
+        
+#player Score circle plot    
 with col3:
     fig2=gauge(t6)
     st.plotly_chart(fig2)
 
+#First set of radar charts
+
 col3, col4 = st.columns(2)
-df1=sp_details.iloc[:,1:8]
+
+
+
+
 with col3:
     #st.subheader('Chart-1')
+    df1=sp_details.iloc[:,:8]
+    df1.drop('TSP Score',axis=1,inplace=True)
     radar_fig1 = radar_chart1(df1)
     exp=st.expander(label='Overall Attributes',expanded=False)
     config ={'displaymode': False}
@@ -366,11 +412,14 @@ with col3:
 with col4:
     #st.subheader('Chart-2')
     exp=st.expander(label='Technical Attributes',expanded=False)
-    df=sp_details.iloc[:,8:17]
+    colsp=sp_details.columns[8:17]
+    colsp=colsp.insert(0,sp_details.columns[0])
+    df=sp_details[colsp]
+    
+    #df=sp_details.iloc[:,8:17]
     df.columns=df.columns.str.title()
-    radio_options=list(df.columns)
-    #radio_options=[x[:5] for x in radio_options]
-    #radio_options=[x.title() for x in radio_options]
+    radio_options=list(df.columns[1:])
+    
     radar_fig1 = radar_chart1(df)
     exp.plotly_chart(radar_fig1,config=config)
 if not selplayers[0]=='Ideal Left Winger' :
@@ -379,36 +428,37 @@ if not selplayers[0]=='Ideal Left Winger' :
             horizontal=True,
             index=None )
     if func1 in radio_options :
-        #st.write(func1)
-        #st.write(selplayers[0])
-        #st.write(techdf.loc[selplayers[0]],[func1])
-        #st.write(techdf)
         
-        bar_data=techdf.loc[selplayers[0]][func1]
-        #st.write(bar_data)
+        series1=techdf.loc[selplayers[0]][func1]
+        series2=techdf.loc['Jack Clarke'][func1]
+        #bar_data=techdf.loc[selplayers[0]][func1]
         exp3=st.expander(label='Bar chart',expanded=True)
-        exp3.plotly_chart(bar_chart((bar_data),func1))
+        #exp3.plotly_chart(bar_chart((bar_data),func1))
+        exp3.plotly_chart(comparison_bar_chart(series1,series2,selplayers[0],'Jack Clarke'))
+
     else:
         pass
-#exp3=st.expander(label='Bar chart',expanded=True)
-#exp3.plotly_chart(bar_chart((bar_data),func1))
 
-#middle table
-
-#dfsel=mid_table['selected_rows']
-#st.write(dfsel.iloc[0,0])
-# Second Row
 # Third Row
-middf['TSP Score']=middf['TSP Score'].astype(str)
+#psdf
 #st.table(middf.iloc[0:5])
-midtab=middf
+midtab=psdf
 midtab.set_index('Player',inplace =True)
+#to round the float value in to integer
+midtab['TSP Score']=psdf['TSP Score'].round().astype(int)
+#to remove the decimal values in string
+midtab['Age']=midtab['Age'].apply(lambda x: x.split('.')[0])
+
 st.table(midtab.iloc[0:5])
 col5, col6= st.columns(2)
 
 with col5:
     #st.subheader('Chart-3')
-    df=sp_details.iloc[:,17:23]
+    #df=sp_details.iloc[:,17:23]
+    
+    colsp=sp_details.columns[17:23]
+    colsp=colsp.insert(0,sp_details.columns[0])
+    df=sp_details[colsp]
     radio2_options=list(df.columns)[1:]
     #radio2_options=[x[:4] for x in radio2_options]
     #radar_fig1 = radar_chart1(df)
@@ -427,12 +477,14 @@ if not selplayers[0]=='Ideal Left Winger' :
         #st.write(selplayers[0])
         #st.write(techdf.loc[selplayers[0]],[func1])
         #st.write(techdf)
-        
+        series1=tactdf.loc[selplayers[0]][func2]
+        series2=tactdf.loc['Jack Clarke'][func2]
             
-        bar_data=tactdf.loc[selplayers[0]][func2]
+        #bar_data=tactdf.loc[selplayers[0]][func2]
         #st.write(bar_data)
         exp3=st.expander(label='Bar chart',expanded=True)
-        exp3.plotly_chart(bar_chart((bar_data),func2))
+        #exp3.plotly_chart(bar_chart((bar_data),func2))
+        exp3.plotly_chart(comparison_bar_chart(series1,series2,selplayers[0],'Jack Clarke'))
     else:
         pass          
 with col6:
@@ -443,4 +495,51 @@ with col6:
     exp=st.expander(label='Psychological Attributes',expanded=False)
     radar_fig1 = radar_chart1(df1)
     exp.plotly_chart(radar_fig1)
+    
+
+
+"""
+
+def comparison_bar_chart(series1, series2, name1,name2):
+    fig = go.Figure()
+
+    # Add trace for the first player
+    fig.add_trace(go.Bar(
+        y=series1.index,
+        x=list(series1),
+        name=name1,
+        orientation='h',
+        text=list(series1),
+        textposition='inside',
+        textangle=0,
+        insidetextanchor='middle',
+        textfont={'size': 20},
+        marker=dict(color='blue')  # Color for the first player
+    ))
+
+    # Add trace for the second player
+    fig.add_trace(go.Bar(
+        y=series2.index,
+        x=list(series2),
+        name=name2,
+        orientation='h',
+        text=list(series2),
+        textposition='inside',
+        textangle=0,
+        insidetextanchor='middle',
+        textfont={'size': 20},
+        marker=dict(color='orange')  # Color for the second player
+    ))
+
+    fig.update_layout(
+        barmode='group',  # Use 'group' mode to place bars side-by-side
+        height=600,
+        title='Player Comparison',
+        xaxis=dict(title='Value'),
+        yaxis=dict(title='Attributes')
+    )
+
+    return fig
+
+"""
 
